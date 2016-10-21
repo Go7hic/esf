25f90a4614598f6357a9ce9634892d44cea78407
Go7hic <gtfx0209@qq.com>
2016-10-22 00:53:16 +0800

    add eslint

--- esh.js
+++ esh.js
@@ -1,4 +1,4 @@
-// Array
+// 数组方法
 export const combine = (...arrays) => [].concat(...arrays)
 
 export const compact = arr => arr.filter(Boolean)
@@ -37,23 +37,23 @@ export const unique = arr => [...Set(arr)]
 export const without = (arr, ...values) => arr.filter(el => !values.some(exclude => el === exclude))
 
 
-// object 
+// 对象方法
 export const getValues = obj => Object.keys(obj).map(key => obj[key])
 
 export const merge = (() => {
   const extend = Object.assign ? Object.assign : (target, ...sources) => {
     sources.forEach(source =>
       Object.keys(source).forEach(prop => target[prop] = source[prop])
-    );
-    return target;
-  };
+    )
+    return target
+  }
   return (...objects) => extend({}, ...objects)
 })()
 
 export const toMap = (() => {
-  const convert = obj => new Map(Object.keys(obj).map(key => [key, obj[key]]));
-  return obj => obj instanceof Map ? obj : convert(obj);
-})();
+  const convert = obj => new Map(Object.keys(obj).map(key => [key, obj[key]]))
+  return obj => obj instanceof Map ? obj : convert(obj)
+})()
 
 
 // math
@@ -71,24 +71,24 @@ export const not = fn => (...args) => !fn(...args)
 
 export const maybe = fn =>
   (...args) => {
-    if (args.length < fn.length || args.some(arg => arg == null)) return;
-    return fn(...args);
-  };
+    if (args.length < fn.length || args.some(arg => arg == null)) return
+    return fn(...args)
+  }
 
-export const once = fn => {
-  var done = false;
+export const once = (fn) => {
+  var done = false
   return (...args) => {
-    if (done) return;
-    done = true;
-    fn(...args);
-  };
+    if (done) return
+    done = true
+    fn(...args)
+  }
 }
 
-export const curry = fn => {
-  const arity = fn.length;
+export const curry = (fn) => {
+  const arity = fn.length
   const curried = (...args) =>
-    args.length < arity ? (...more) => curried(...args, ...more) : fn(...args);
-  return curried;
+    args.length < arity ? (...more) => curried(...args, ...more) : fn(...args)
+  return curried
 }
 
 export const pipeline = (...funcs) => value => funcs.reduce((a, b) => b(a), value)
